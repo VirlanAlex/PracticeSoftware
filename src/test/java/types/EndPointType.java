82b5891467b0108a77cb04579b93de019e9b2e1f
Alex Virlan <virlanalexandru20@yahoo.com>
2026-03-12 08:25:00 +0200

    update

--- src/test/java/types/EndPointType.java
+++ src/test/java/types/EndPointType.java
@@ -11,9 +11,8 @@ public class EndPointType {
     public static final String REPORT_AVERAGE_SALES_ENDPOINT = "/reports/average-sales-per-month";
 
     public static final String USER_CREATE_ENDPOINT = "/users/register";
-    public static final String USER_LOGIN_ENDPOINT = "/users/login";
-    public static final String USER_SPECIFIC_ENDPOINT = "/users/";
-    public static final String USER_LOGOUT_ENDPOINT = "/users/logout";
-
+    public static final String USER_LOGIN_ENDPOINT = "users/login";
+    public static final String USER_SPECIFIC_ENDPOINT = "users/";
+    public static final String USER_LOGOUT_ENDPOINT = "users/logout";
 
 }
